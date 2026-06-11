7dd436a075ff7f3caebc1b1c13f11c8872c7d2dd
overlordkdone <overlordkdone@github.com>
2026-06-11 21:47:01 +1200

    Implement macOS installer detection in OpenDbvX64Dxe driver
    - Add OcGetDbtBootEntries to scan for com.apple.installer directory
    - Install gOcBootEntryProtocolGuid for boot entry integration

--- Platform/OpenDbvX64Dxe/OpenDbvX64Dxe.c
+++ Platform/OpenDbvX64Dxe/OpenDbvX64Dxe.c
@@ -1,15 +1,156 @@
 /** @file
-  Copyright (C) 2026. All rights reserved.
+   Copyright (C) 2026. All rights reserved.
 
-  Dynamic Binary Translation DXE Driver for ARM64 to x86_64
+   Dynamic Binary Translation DXE Driver for ARM64 to x86_64
 **/
 
 #include <Uefi.h>
 #include <Library/BaseLib.h>
+#include <Library/BaseMemoryLib.h>
 #include <Library/DebugLib.h>
+#include <Library/DevicePathLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/PrintLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
 #include <Library/OcDbtLib.h>
+#include <Library/OcBootManagementLib.h>
+
+#include <Protocol/OcBootEntry.h>
+
+STATIC DBT_CONTEXT  *gDbtContext = NULL;
+
+STATIC
+EFI_STATUS
+EFIAPI
+OcGetDbtBootEntries (
+  IN OUT         OC_PICKER_CONTEXT  *PickerContext,
+  IN     CONST EFI_HANDLE           Device OPTIONAL,
+  OUT       OC_PICKER_ENTRY         **Entries,
+  OUT       UINTN                   *NumEntries
+  )
+{
+  EFI_STATUS                       Status;
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL  *FileSystem;
+  EFI_FILE_PROTOCOL                *RootDirectory;
+  EFI_FILE_PROTOCOL                *BootDirectory;
+  EFI_FILE_INFO                    *FileInfo;
+  UINTN                            FileInfoSize;
+  UINTN                            EntryCount;
+  OC_PICKER_ENTRY                  *NewEntries;
+
+  ASSERT (PickerContext != NULL);
+  ASSERT (Entries != NULL);
+  ASSERT (NumEntries != NULL);
+
+  *Entries    = NULL;
+  *NumEntries = 0;
+
+  if (Device == NULL) {
+    return EFI_NOT_FOUND;
+  }
+
+  Status = gBS->HandleProtocol (
+                  Device,
+                  &gEfiSimpleFileSystemProtocolGuid,
+                  (VOID **)&FileSystem
+                  );
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  Status = FileSystem->OpenVolume (FileSystem, &RootDirectory);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  EntryCount = 0;
+
+  //
+  // Look for macOS Installer (com.apple.installer) in boot directories
+  //
+  Status = RootDirectory->Open (
+                          RootDirectory,
+                          &BootDirectory,
+                          L"\\System\\Library\\CoreServices\\com.apple.installer",
+                          EFI_FILE_MODE_READ,
+                          0
+                          );
+
+  if (!EFI_ERROR (Status)) {
+    Status = EFI_NOT_FOUND;
+
+    FileInfoSize = 0;
+    BootDirectory->GetInfo (BootDirectory, &gEfiFileInfoGuid, &FileInfoSize, NULL);
+    if (FileInfoSize > 0) {
+      Status = EFI_SUCCESS;
+      FileInfo = AllocatePool (FileInfoSize);
+      if (FileInfo != NULL) {
+        BootDirectory->GetInfo (BootDirectory, &gEfiFileInfoGuid, &FileInfoSize, FileInfo);
+        if ((FileInfo->Attribute & EFI_FILE_DIRECTORY) != 0) {
+          ++EntryCount;
+        }
+        FreePool (FileInfo);
+      }
+    }
+    BootDirectory->Close (BootDirectory);
+  }
+
+  RootDirectory->Close (RootDirectory);
+
+  if (EntryCount > 0) {
+    NewEntries = AllocatePool (sizeof (OC_PICKER_ENTRY) * EntryCount);
+    if (NewEntries == NULL) {
+      return EFI_OUT_OF_RESOURCES;
+    }
+
+    ZeroMem (NewEntries, sizeof (OC_PICKER_ENTRY) * EntryCount);
+
+    NewEntries[0].Id      = "macOS-Installer";
+    NewEntries[0].Name    = "macOS Installer (Translated)";
+    NewEntries[0].Flavour = "OpenDbt";
+
+    *Entries    = NewEntries;
+    *NumEntries = EntryCount;
+    return EFI_SUCCESS;
+  }
+
+  return EFI_NOT_FOUND;
+}
+
+STATIC
+VOID
+EFIAPI
+OcFreeDbtBootEntries (
+  IN  OC_PICKER_ENTRY  **Entries,
+  IN  UINTN            NumEntries
+  )
+{
+  UINTN  Index;
+
+  if ((Entries == NULL) || (*Entries == NULL)) {
+    return;
+  }
+
+  for (Index = 0; Index < NumEntries; Index++) {
+    if ((*Entries)[Index].Id != NULL) {
+      FreePool ((CHAR8 *)(*Entries)[Index].Id);
+    }
+    if ((*Entries)[Index].Name != NULL) {
+      FreePool ((CHAR8 *)(*Entries)[Index].Name);
+    }
+  }
+
+  FreePool (*Entries);
+  *Entries = NULL;
+}
+
+STATIC OC_BOOT_ENTRY_PROTOCOL  mDbtBootEntryProtocol = {
+  OC_BOOT_ENTRY_PROTOCOL_REVISION,
+  OcGetDbtBootEntries,
+  OcFreeDbtBootEntries,
+  NULL
+};
 
 EFI_STATUS
 EFIAPI
@@ -18,16 +159,30 @@ OpenDbvX64EntryPoint (
   IN EFI_SYSTEM_TABLE  *SystemTable
   )
 {
-  EFI_STATUS     Status;
-  DBT_CONTEXT    *DbtContext;
+  EFI_STATUS                 Status;
 
-  Status = DbtInitContext (&DbtContext, 0x10000);
-
+  Status = DbtInitContext (&gDbtContext, 0x10000);
   if (EFI_ERROR (Status)) {
     return Status;
   }
 
   DEBUG ((DEBUG_INFO, "DBT ARM64->x86_64 initialized\n"));
+
+  //
+  // Install boot entry protocol to provide installer entries
+  //
+  Status = gBS->InstallMultipleProtocolInterfaces (
+                  &ImageHandle,
+                  &gOcBootEntryProtocolGuid,
+                  &mDbtBootEntryProtocol,
+                  NULL
+                  );
+
+  if (EFI_ERROR (Status)) {
+    DbtFreeContext (gDbtContext);
+    gDbtContext = NULL;
+    return Status;
+  }
 
   return EFI_SUCCESS;
 }
